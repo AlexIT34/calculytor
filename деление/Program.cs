8eddc3eb1a314ac39918716f1dcf02aef1331e3c
Alex <asdsu@example.com>
2022-10-12 00:01:41 +0300

    калькулятор

--- деление/Program.cs
+++ деление/Program.cs
@@ -3,6 +3,10 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число");
 int b = Convert.ToInt32(Console.ReadLine());
 int c = a / b;
+{
+    int c = a + b;
 
-Console.Write("деление 1 и 2 числа = ");
-Console.WriteLine(c); 
+
+    Console.Write("сумма = ");
+    Console.WriteLine(c);
+}
---
+++ калькулятор/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Что бы произвести сложение введите 0; вычитание 1; умножение 2; деление 3");
+int g = Convert.ToInt32(Console.ReadLine());
+int d = a + b;
+int f = a - b;
+int e = a * b;
+int j = a / b;
+
+
+if (g == 0)
+{
+    Console.Write("= ");
+    Console.WriteLine(d);
+}    
+    
+if (g == 1)
+{   
+    Console.Write("= ");
+    Console.WriteLine(f);
+}   
+
+if (g == 2)
+{  
+    Console.Write("= ");
+    Console.WriteLine(e);
+}  
+
+if (g == 3)
+{  
+    Console.Write("= ");
+    Console.WriteLine(j);
+}  
