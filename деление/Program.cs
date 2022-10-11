862cbc62d254396377b4fa0ae37ff9d893fa828c
Alex <asdsu@example.com>
2022-10-11 22:41:28 +0300

    деление

---
+++ деление/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a / b;
+
+Console.Write("деление 1 и 2 числа = ");
+Console.WriteLine(c); 
