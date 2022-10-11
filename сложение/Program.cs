47934f96f12237f8a0cb27784aef2e218a835e4a
Alex <asdsu@example.com>
2022-10-11 22:13:24 +0300

    сложение

---
+++ сложение/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a + b;
+
+
+Console.Write("сумма = ");
+Console.WriteLine(c);
